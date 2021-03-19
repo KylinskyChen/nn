ea3644fddb9b6678213791144782d3bfb6304e2e
jerome <drchenxie@163.com>
2021-03-19 17:02:40 +0800

    BOSS 回来了，先干活，晚上回去继续写～

--- rust/0.neuron/neuron/src/main.rs
+++ rust/0.neuron/neuron/src/main.rs
@@ -1,40 +1,47 @@
+/*
+ * 作者：解琛；
+ * 功能：基本神经元；
+ * 时间：2021 年 3 月 19 日；
+ */
 
-// pub struct Neuron {
-//     weight: f32,
-// 	bias:   f32
-// }
+pub struct Neuron {
+    weight: f32,
+	bias:   f32
+}
 
-// impl Neuron {
+impl Neuron {
 
-//     pub fn new(weight: f32, bias: f32) -> Neuron {
-//         Neuron {
-//             weight: weight,
-//             bias:   bias
-//         }
-//     }
+    pub fn new(weight: f32, bias: f32) -> Neuron {
+        Neuron {
+            weight: weight,
+            bias:   bias
+        }
+    }
 
-//     fn setWeight(&self, weight: f32) {
-//         self.weight = weight
-//     }
+    fn setWeight(&self, weight: f32) {
+        self.weight = weight
+    }
 
-//     fn getWeight(&self) -> f32 {
-//         return self.weight
-//     }
+    fn getWeight(&self) -> f32 {
+        return self.weight
+    }
 
-//     fn setBias(&self, bias: f32) {
-//         self.bias = bias
-//     }
+    fn setBias(&self, bias: f32) {
+        self.bias = bias
+    }
 
-//     fn getBias(&self) -> f32 {
-//         return self.bias
-//     }
+    fn getBias(&self) -> f32 {
+        return self.bias
+    }
 
-//     pub feedforward(&self, inputs: f32; 4) {
+    fn 
 
-//     }
+    // pub feedforward(&self, inputs: f32; 4) {
+
+    // }
 
 
-// }
+}
 
 
 fn main() {
